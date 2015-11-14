416993f0df11fb0453a0223abaf2a73c5e6a5c58
Scott Meade <scott@synapsoftware.com>
2015-11-14 08:25:44 -0700

    Hook up the data model

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,9 +1,14 @@
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
       </div>
     );
   }
@@ -11,10 +16,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return(
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Joe J">Comment *one*.</Comment>
-        <Comment author="Jane W">Comment *two*.</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -48,6 +59,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data}/>,
   document.getElementById('content')
 );
